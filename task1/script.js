3dd980b9fb8856db593560332f3acdd5ae890c36
juancopi81 <juancopi_81@hotmail.com>
2021-03-06 09:58:42 -0500

    Add changes to script.js to capture values of the form

--- task1/script.js
+++ task1/script.js
@@ -1,8 +1,30 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
+// Variable to keep track of notes
+let notes = [];
+
 // When document is ready
 document.addEventListener('DOMContentLoaded', () => {
+  
+  // Disable some options based on user's selection
+  document.getElementById('duration').addEventListener('change', function(obj) {
+    
+    // Retrive the information sent by the user
+    let inputPitch = document.getElementById('pitch');
+    let inputAccidental = document.getElementById('accidental');
+    let inputOctave = document.getElementById('octave');
+    let inputDot = document.getElementById('dotted');
+    
+    // Disable pitch if is rest
+    inputPitch.disabled = (this.value.includes('r'));
+    inputAccidental.disabled = (this.value.includes('r'));
+    inputOctave.disabled = (this.value.includes('r'));
+    
+    // Disabel dot if is 16 or 1 note
+    inputDot.disabled = (this.value.includes('16') || this.value.includes('1'));
+    
+  });
   
   // When the input form is submitted
   document.querySelector('#input-form').onsubmit = (e) => {
@@ -11,9 +33,26 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     
     // Retrive the information sent by the user
-    
+    let inputDuration = document.getElementById('duration').value;
     let inputPitch = document.getElementById('pitch').value;
     let inputAccidental = document.getElementById('accidental').value;
     let inputOctave = document.getElementById('octave').value;
+    
+    // Concatenate
+    let inputNote = inputPitch + inputAccidental + inputOctave
+    
+    // Check if note is dotted
+    let isDotted = false;
+    
+    if (document.getElementById('dotted').checked == true) {
+      isDotted = true;
+      inputDuration += '.';
+    }
+    
+    // Add note to the array
+    notes = [...notes, {'duration': inputDuration, 'note': inputNote, 'isDotted': isDotted}];
+    
+    console.log(notes);
+    
   }
 })
