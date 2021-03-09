dd410b356c19a5f6ae50072fc317721aee76a8dd
juancopi81 <juancopi_81@hotmail.com>
2021-03-09 16:04:08 -0500

    Add task 4

--- task4/README.md
+++ task4/README.md
@@ -1,4 +1,23 @@
-## Task 4
+# Task 5: Adding the notes to VexFlow 
 
-## Learning objective:
-By the end of Task 4, you will be able to create a parser that takes the note from the form and converts it into a readable format that Vexflow can understand
+## Learning objective: 
+By the end of Task 5, you will be able to add the notes from the parser to the staff created in Vexflow
+
+## Script: 
+Welcome back. In this task; we will use our parser to add the notes to the staff created using EasyScore
+So let’s go to the end of our JS file and add a function call [function parseNotesToVex(notes)]
+[createVariable] + [loop over each notes] + [createVariables] + [is a rest or not] + [case is a rest] + [cas is not a rest]
+Debug using chrome 
+
+## Quiz Questions:
+In JavaScript, how can we check if a string is present within another string?
+1. if substring in string:
+2. string.includes(substring)
+3. substring.in(string)
+4. None of the above
+
+The line in our parser ‘for (let note of notes)’:
+1. It is a JavaScript syntax for looping
+2. It is a JavaScript syntax for conditions
+3. 1 and 2
+4. None of the above
---
+++ task4/script.js
@@ -0,0 +1,133 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// Variable to keep track of notes
+let notes = [];
+
+// Variable to keep track of render notes
+const notesEmptyStaff = 'B4/4/r, B4/4/r, B4/4/r, B4/4/r';
+
+// Vexflow variable
+let VF = Vex.Flow;
+
+// When document is ready
+document.addEventListener('DOMContentLoaded', () => {
+  
+  // Create a new staff
+  createStaff(notesEmptyStaff);
+  
+  // Disable some options based on user's selection
+  document.getElementById('duration').addEventListener('change', function(obj) {
+    
+    // Retrive the information sent by the user
+    let inputPitch = document.getElementById('pitch');
+    let inputAccidental = document.getElementById('accidental');
+    let inputOctave = document.getElementById('octave');
+    let inputDot = document.getElementById('dotted');
+    
+    // Disable pitch if is rest
+    inputPitch.disabled = (this.value.includes('r'));
+    inputAccidental.disabled = (this.value.includes('r'));
+    inputOctave.disabled = (this.value.includes('r'));
+    
+    // Disabel dot if is 16 or 1 note
+    inputDot.disabled = (this.value.includes('16') || this.value.includes('1'));
+    
+  });
+  
+  // When the input form is submitted
+  document.querySelector('#input-form').onsubmit = (e) => {
+    
+    // Prevent the form to be sent
+    e.preventDefault();
+    
+    // Retrive the information sent by the user
+    let inputDuration = document.getElementById('duration').value;
+    let inputPitch = document.getElementById('pitch').value;
+    let inputAccidental = document.getElementById('accidental').value;
+    let inputOctave = document.getElementById('octave').value;
+    
+    // Concatenate
+    let inputNote = inputPitch + inputAccidental + inputOctave
+    
+    // Check if note is dotted
+    let isDotted = false;
+    
+    if (document.getElementById('dotted').checked == true) {
+      isDotted = true;
+      inputDuration += '.';
+    }
+    
+    // Add note to the array
+    notes = [...notes, {'duration': inputDuration, 'pitch': inputNote, 'isDotted': isDotted}];
+  }
+})
+
+// Create a staff using Vexflow with renderNotes
+function createStaff(renderNotes) {
+  
+  // Remove any old staff
+  const staff = document.getElementById('staff');
+  
+  while(staff.hasChildNodes()) {
+    staff.removeChild(staff.lastChild);
+  }
+  
+  // Create an SVG renderer and attach it to the DIV element named 'staff"
+  var vf = new VF.Factory({renderer: {elementId: 'staff'}});
+  var score = vf.EasyScore();
+  var system = vf.System();
+  
+  // Add a staff and voices to our system
+  system.addStave({
+    voices: [score.voice(score.notes(renderNotes))]
+  }).addClef('treble').addTimeSignature('4/4');
+  
+  vf.draw();
+}
+
+// Parse notes to renderNotes
+function parseNotesToVex(notes) {
+  
+  // Create empty string
+  let notesString = '';
+  
+  // Loop over the notes
+  for (let note of notes) {
+    
+    // Empty string of the note
+    let noteString = '';
+    let noteDuration;
+    
+    // Check if note is a rest
+    if (note.duration.includes('r')) {
+      
+      // Remove the r 
+      noteDuration = note.duration.replace('r', '');
+      
+      // Check if silences is dotted 
+      if (note.isDotted) {
+        
+        // Remove dot and add it to the right position
+        noteString = noteDuration.replace('.', '');
+        noteString = 'B4/' + noteString + 'r.,';
+        
+      } else {
+        
+        // Create the string for the rest
+        noteString = 'B4/' + noteDuration + '/r,';
+      }
+      
+    } else {
+      
+      // If not rest
+      noteString = note.pitch + '/' + note.duration + ',';
+      noteDuration = note.duration;
+      
+    } 
+    
+    notesString += noteString;
+  }
+  
+  return notesString;
+}
